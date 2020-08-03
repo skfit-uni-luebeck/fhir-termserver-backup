81cfc66c94fdecd6747aae2619e7d7dad67f7f4f
Joshua Wiedekopf <jpwiedekopf@gmail.com>
2020-08-03 15:51:55 +0200

    add documentation

--- backup.py
+++ backup.py
@@ -8,6 +8,14 @@ from os import path, makedirs
 
 
 def parse_args(print_args: bool = True) -> argparse.Namespace:
+    """create the argument parser
+
+    Args:
+        print_args (bool, optional): If true, the arguments will be printed to stdout after parsing. Defaults to True.
+
+    Returns:
+        argparse.Namespace: the parsed arguments as a Namespace object
+    """
     default_url = "http://localhost:8080/fhir"
 
     default_resource_types = ["CodeSystem", "ValueSet", "ConceptMap"]
@@ -51,6 +59,8 @@ def parse_args(print_args: bool = True) -> argparse.Namespace:
 
 @dataclass
 class BundleResponse:
+    """store the entry in a bundle GET operation
+    """
     resource_id: str
     title: str
     canonical_url: str
@@ -58,7 +68,19 @@ class BundleResponse:
     version: str
 
 
-def perform_request_as_json(url: str, headers: List[str] = None) -> Any:
+def perform_request_as_json(url: str, headers: List[str] = None) -> Dict[str, Any]:
+    """perform a GET request to url with headers and return the JSON representation of the response
+
+    Args:
+        url (str): the endpoint to query
+        headers (List[str], optional): the headers to pass to requests.get. Defaults to None.
+
+    Raises:
+        ValueError: if the status code is not 200
+
+    Returns:
+        Dict[str, Any]: The JSON body as a Dict
+    """
     rx = requests.get(url, headers=headers)
     if rx.status_code != 200:
         raise ValueError(
@@ -67,6 +89,16 @@ def perform_request_as_json(url: str, headers: List[str] = None) -> Any:
 
 
 def get_resource_urls_from_server(fhir_endpoint: str, resource_type: str, headers: List[str] = None) -> List[BundleResponse]:
+    """get the urls of all the resources of the given type from the server. Walks through bundles!
+
+    Args:
+        fhir_endpoint (str): the endpoint
+        resource_type (str): the resource type (gets included in the request url)
+        headers (List[str], optional): the headers to pass to requests.get. Defaults to None.
+
+    Returns:
+        List[BundleResponse]: The parsed entries of the original bundle
+    """
     request_url = f"{fhir_endpoint}/{resource_type}"
     bundle_responses: List[BundleResponse] = []
     next_link = request_url
@@ -80,14 +112,30 @@ def get_resource_urls_from_server(fhir_endpoint: str, resource_type: str, header
     return bundle_responses
 
 
-def bundle_json_get_next_link(bundle_json: Any) -> str:
+def bundle_json_get_next_link(bundle_json: Dict[str, Any]) -> str:
+    """get the 'next' link from a bundle, if it exists
+
+    Args:
+        bundle_json (Dict[str, Any]): the bundle to examine
+
+    Returns:
+        str: the url of the 'next' bundle or None
+    """
     filtered_link = [l for l in bundle_json["link"] if l["relation"] == "next"]
     if (len(filtered_link) > 0):
         return filtered_link[0]["url"]
     return None
 
 
-def bundlejson_to_bundle_response_list(bundle_json: Any) -> List[BundleResponse]:
+def bundlejson_to_bundle_response_list(bundle_json: Dict[str, Any]) -> List[BundleResponse]:
+    """parse every entry in the bundle as a BundleResponse
+
+    Args:
+        bundle_json (Dict[str, Any]): the bundle to parse
+
+    Returns:
+        List[BundleResponse]: the parsed entries
+    """
     responses: List[BundleResponse] = []
     for entry in bundle_json['entry']:
         url = entry["fullUrl"]
@@ -106,6 +154,17 @@ def bundlejson_to_bundle_response_list(bundle_json: Any) -> List[BundleResponse]
 
 
 def download_resource(resource_type: str, r: BundleResponse, out_dir: str, today: str) -> str:
+    """download a resource from the fully-qualified url in r to out_dir
+
+    Args:
+        resource_type (str): the resource type, used in the output filename
+        r (BundleResponse): the parsed entry from the search bundle
+        out_dir (str): the output directory
+        today (str): the current date (or anything else), used in the output filename. Lifted out to avoid changing filenames around midnight local time.
+
+    Returns:
+        str: the fully-qualified output path
+    """
     rx = perform_request_as_json(r.url)
 
     target_filename = f"{resource_type}-{r.resource_id}_{r.title}_{today}.json"
@@ -115,7 +174,26 @@ def download_resource(resource_type: str, r: BundleResponse, out_dir: str, today
     return target_abspath
 
 
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    via https://stackoverflow.com/a/295466
+
+    """
+    import unicodedata
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    value = unicode(re.sub('[-\s]+', '-', value))
+    return value
+
+
 def download_all_resource_types(args: argparse.Namespace):
+    """main entry point into the app
+
+    Args:
+        args (argparse.Namespace): the parsed arguments
+    """
     today = date.today().isoformat()
     for resource_type in args.resource_types:
         print("\n\n########\n\n")
